b414d5d3aa62a2f393eac7203a56336053e33f25
Marcelo Bravo <mabc@live.cl>
2025-06-16 19:31:18 -0400

    Se creó el proyecto action-demo

---
+++ sum.js
@@ -0,0 +1,15 @@
+/**
+ * Calcula la suma de dos números.
+ *
+ * @param {number} a - El primer número.
+ * @param {number} b - El segundo número.
+ * @returns {number} La suma de los dos números.
+ */
+function sum(a, b) {
+ return a + b;
+}
+
+/**
+ * Exporta la función sum para su uso en otros módulos.
+ */
+module.exports = sum;
---
+++ sum.test.js
@@ -0,0 +1,18 @@
+/**
+ * Importa la función sum desde el archivo sum.js.
+ */
+const sum = require('./sum');
+
+/**
+ * Prueba unitaria para verificar la suma de dos números positivos.
+ */
+test('Suma dos números', () => {
+ expect(sum(1, 2)).toBe(3);
+});
+
+/**
+ * Prueba unitaria para verificar la suma de dos números negativos.
+ */
+test('Suma de números negativos', () => {
+ expect(sum(-1, -2)).toBe(-3);
+});
